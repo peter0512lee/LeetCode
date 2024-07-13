585152610f25e7cf3377e19b5aad89319c2379f9
Jie-Ying Li <peter0512lee@gmail.com>
2024-07-13 10:24:31 +0800

    Time: 228 ms (75.47%), Space: 18.1 MB (60.38%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,30 +1,20 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-		# to calculate first, high value of x or y
-        a, b = 'ab', 'ba'
-        if y > x:
-            b, a, y, x = a, b, x, y
-
-        answer = 0
-        
-        for word in [a, b]:
+        def remove_pairs(pair, score):
+            nonlocal s
+            res = 0
             stack = []
+            for c in s:
+                if c == pair[1] and stack and stack[-1] == pair[0]:
+                    stack.pop()
+                    res += score
+                else:
+                    stack.append(c)
+            s = "".join(stack)
+            return res
 
-            i = 0
-            while i < len(s):
-                stack.append(s[i])
-                
-                n = len(stack)
-                prefix = stack[n-2] + stack[n-1]
-                # if see the prefix ab or ba move from stack and increment the answer
-                if prefix == word:
-                    answer += x
-                    stack.pop()
-                    stack.pop()
-                i += 1
-            # change the x point to y for 2nd iteration
-            x = y
-            
-            # assign new letters with already removed prefix
-            s = ''.join(stack)
-        return answer
+        res = 0
+        pair = "ab" if x > y else "ba"
+        res += remove_pairs(pair, max(x, y))
+        res += remove_pairs(pair[::-1], min(x, y))
+        return res
