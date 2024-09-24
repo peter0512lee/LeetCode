86025e41ab49f5d0e2aedbbde73803d39eb033da
Jie-Ying Li <peter0512lee@gmail.com>
2024-09-24 15:25:47 +0800

    Time: 244 ms (58.65%), Space: 18.9 MB (61.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-
-        islands = 0
-        visit = set()
-        rows, cols = len(grid), len(grid[0])
-
-        def dfs(r, c):
-            if (
-                r not in range(rows)
-                or c not in range(cols)
-                or grid[r][c] == "0"
-                or (r, c) in visit
-            ):
+        
+        def dfs(grid, row, col):
+            if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[row]) or grid[row][col] == "0":
                 return
 
-            visit.add((r, c))
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+            grid[row][col] = "0"
+            
+            dfs(grid, row + 1, col)
+            dfs(grid, row, col + 1)
+            dfs(grid, row - 1, col)
+            dfs(grid, row, col - 1)
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
+                    dfs(grid, row, col)
                     islands += 1
-                    dfs(r, c)
+
         return islands
