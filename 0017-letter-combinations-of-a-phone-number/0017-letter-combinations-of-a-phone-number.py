7de7c6c82799962a5a287db0ca105455b695b877
Jie-Ying Li <peter0512lee@gmail.com>
2024-10-15 23:01:34 +0800

    Time: 32 ms (75.81%), Space: 16.6 MB (17.96%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,18 +1,22 @@
-MAPPING = "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        mapping = "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+
         n = len(digits)
+
         if n == 0:
             return []
+            
         ans = []
-        path = [''] * n  # 本题 path 长度固定为 n
-        def dfs(i: int) -> None:
+        path = [''] * n
+        def dfs(i):
+            # 邊界條件
             if i == n:
                 ans.append(''.join(path))
                 return
-            for c in MAPPING[int(digits[i])]:
-                path[i] = c  # 直接覆盖
-                dfs(i + 1)
+            # 遞條件
+            for c in mapping[int(digits[i])]:
+                path[i] = c
+                dfs(i+1)
         dfs(0)
         return ans
