b70049dc16bf53e56c09d16d0104aa63f5fd7e52
Jie-Ying Li <peter0512lee@gmail.com>
2024-11-16 15:03:10 +0800

    Time: 90 ms (12.23%), Space: 16.7 MB (55.71%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -1,21 +1,16 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        def is_consecutive_and_sorted(subarray):
+            return all(subarray[i] + 1 == subarray[i + 1] for i in range(len(subarray) - 1))
+
         n = len(nums)
-        results = []
-        
-        # For each possible subarray of size k
+        res = []
+
         for i in range(n - k + 1):
             subarray = nums[i:i + k]
-            
-            is_sorted = all(subarray[j] < subarray[j + 1] for j in range(len(subarray) - 1))
-            
-            is_consecutive = False
-            if is_sorted:
-                is_consecutive = all(subarray[j + 1] - subarray[j] == 1 for j in range(len(subarray) - 1))
-            
-            if is_sorted and is_consecutive:
-                results.append(max(subarray))
+            if is_consecutive_and_sorted(subarray):
+                res.append(max(subarray))
             else:
-                results.append(-1)
-        
-        return results
+                res.append(-1)
+
+        return res
