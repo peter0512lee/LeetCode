e785182be09ac5be47f60be72a75399fc55fadf8
Jie-Ying Li <peter0512lee@gmail.com>
2024-11-25 22:04:32 +0800

    Time: 3 ms (53.99%), Space: 17.9 MB (14.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        建立一個 num -> idx 的 hashmap，如果剩餘的數有在 map 裡的話，就是答案
+        否則把當前的 num 跟 idx 記錄在 map
+        """
         num_idx_map = {}
         for i, num in enumerate(nums):
             complement = target - num
             if complement in num_idx_map:
-                return [num_idx_map[num], i]
+                return [num_idx_map[complement], i]
             num_idx_map[num] = i
